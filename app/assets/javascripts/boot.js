2e395aa7fba763257fc2dfc8751133e90830269c
Scott Erickson <sderickson@gmail.com>
2015-03-24 11:13:06 -0700

    Switched boot.js back to handle exceptions normally.

--- app/assets/javascripts/boot.js
+++ app/assets/javascripts/boot.js
@@ -114,7 +114,7 @@
     getWindowLocation: function() { return window.location; }
   });
 
-  var catchingExceptions = true; // Turning off because, for some reason, errors in app.js don't print a stack or line number at all
+  var catchingExceptions = queryString.getParam("catch"); // Turning off because, for some reason, errors in app.js don't print a stack or line number at all
   env.catchExceptions(typeof catchingExceptions === "undefined" ? true : catchingExceptions);
 
   /**
